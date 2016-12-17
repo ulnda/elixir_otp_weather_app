f9678d14c2ac8cae28ff9d9b8a738f8ecd7150bc
d.nagibin <d.nagibin@fun-box.ru>
2016-12-17 22:13:00 +0400

    Setup deps and setup prototype of GenServer.

---
+++ lib/worker.ex
@@ -0,0 +1,70 @@
+defmodule OtpMetex.Worker do
+  use GenServer
+
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  ## Server callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:location, location}, from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp} C", new_stats}
+
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
+
+  ## Helper functions
+  defp temperature_of(location) do
+    url_for(location) |> HTTPoison.get |> parse_response
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apiKey}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apiKey do
+    "8b1c441701553b4c30a6b155f15b4cca"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
+  end
+end
